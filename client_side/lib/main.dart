9f285a4787b2132de205233e9570d8c5cd5c9218
Stephen <stephen@Stephens-MacBook-Pro-2.local>
2020-02-01 13:22:06 -0600

    Updated main

--- client_side/lib/main.dart
+++ client_side/lib/main.dart
@@ -83,9 +83,30 @@ class FormPage extends StatefulWidget {
 
 class FormPageState extends State<FormPage>{
   int _painScale = 0;
-  TextEditingController _namecontroller = TextEditingController();
+  TextEditingController _symptomController = TextEditingController();
+  TextEditingController _medicationsController = TextEditingController();
+  //  String text = _symptomController.text;
   //TextEditingController _paincontroller = TextEditingController();
-
+  Future<void> _onClick() async {
+    // Get the text from the entry
+    // Check to make sure that the message is not empty
+    /*if (text == '' || text == ' '){
+      message = "Please enter your message";
+      SystemChannels.textInput.invokeMethod('TextInput.hide');
+      return 1;
+    } else {
+      // Check if the value of _last message is text, this prevents unnesssiary requests
+      // from going to the web server
+      if (_lastMessage == text && _lastPick == _selectedMirror){
+        print("Did not send message ");
+        SystemChannels.textInput.invokeMethod('TextInput.hide');
+        return 1;
+      } else {
+        _lastMessage = text;
+        _lastPick = _selectedMirror;
+      }
+    }*/
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -95,7 +116,7 @@ class FormPageState extends State<FormPage>{
       body: Center(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 20,),
+            SizedBox(height: 50,),
             Text("Enter your information Below!",
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -103,10 +124,16 @@ class FormPageState extends State<FormPage>{
               ),
             ),
             // DropDown Button
+            SizedBox(height: 50,),
             Row(
               children: <Widget>[
-                SizedBox(width: 50,),
-                Text("Select your pain from 1-10"),
+                SizedBox(width: 20,),
+                Text(
+                  "Select your pain from 1-10! \nWith 10 being the most pain",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
                 SizedBox(width: 50,),
                 DropdownButton<int>(
                   items: <int>[1,2,3,4,5,6,7,8,9,10].map((int value) {
@@ -124,12 +151,33 @@ class FormPageState extends State<FormPage>{
               ],),
             SizedBox(
               height: 50
+            ), 
+            TextField(
+              textAlign: TextAlign.center,
+              autocorrect: true,
+              textCapitalization: TextCapitalization.sentences,
+              keyboardType: TextInputType.multiline,
+              maxLines: 2,
+              controller: _symptomController,
+              decoration: InputDecoration(
+                hintText: 'Describe your symptoms here...',
+              ),
             ),
-            
-
+            SizedBox(height: 30,),
+            TextField(
+              textAlign: TextAlign.center,
+              autocorrect: false,
+              textCapitalization: TextCapitalization.words,
+              keyboardType: TextInputType.multiline,
+              maxLines: 1,
+              controller: _medicationsController,
+              decoration: InputDecoration(
+                hintText: 'What medications do you take?',
+              ),
+            ),
           ],
           )
-          ,)
+        )
     );
   }
 }
